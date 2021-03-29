42f045807418c7594f59aff99ab275d7e1accd4f
pxviet1997 <pxviet1997@gmail.com>
2021-03-30 00:57:33 +1100

    check if password has met with requirements

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import '../../App.css';
 import './AuthForm.css';
 import { Link } from 'react-router-dom';
@@ -7,30 +7,62 @@ import { useHistory } from 'react-router-dom';
 
 
 function AuthForm(props) {
-    const emailRef = React.useRef();
-    const passwordRef = React.useRef();
-    const passwordConfirmRef = React.useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
     const { signup, login } = useAuth();
-    const [error, setError] = React.useState('');
-    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
     const history = useHistory();
+    const [passwordError, setPasswordError] = useState('')
+    // const [passwordConformationError, setPasswordConformationError] = useState('')
 
     const alternativeChoice = props.label === 'Sign Up' ? 'Already have an account?' : 'Don\'t have an account?'
     const alternativePath = props.label === 'Sign Up' ? '/log-in' : '/sign-up'
-    const alternativeLabel = props.label === 'Sign Up' ? 'Log In' : 'Sign Up'
+    const alternativeLink = props.label === 'Sign Up' ? 'Log In' : 'Sign Up'
+
+    function signUpCheck() {
+        const isOk1 = new RegExp('[a-z]').test(passwordRef.current.value)
+        const isOk2 = new RegExp('[A-Z]').test(passwordRef.current.value)
+        const isOk3 = new RegExp('[0-9]').test(passwordRef.current.value)
+        const isOk4 = new RegExp(`${passwordRef.current.value}{8,20}`).test(passwordRef.current.value)
+
+        return isOk1 && isOk2 && isOk3 & isOk4
+    }
+
+    function passwordConformationCheck() {
+        return passwordRef.current.value === passwordConfirmRef.current.value
+    }
 
     async function handleSubmit(e) {
         e.preventDefault();
         try {
             setError('');
             setLoading(true);
-            await props.label === 'Sign Up' ? signup(emailRef.current.value, passwordRef.current.value)
-                : login(emailRef.current.value, passwordRef.current.value);
-            history.push('/');
+            if (props.label === 'Sign Up') {
+                if (!passwordConformationCheck()) {
+                    setPasswordError('Password Conformation does not match!')
+                }
+                else {
+                    if (!signUpCheck()) {
+                        setPasswordError('Password does not meet requirements')
+                    }
+                    else {
+                        await signup(emailRef.current.value, passwordRef.current.value)
+                        history.push('/')
+                    }
+                }
+            }
+            else {
+                await login(emailRef.current.value, passwordRef.current.value)
+                history.push('/')
+            }
+
         } catch {
             setError(`Failed to ${props.label}`)
         }
-        setLoading(false);
+        setLoading(false)
+
     }
 
     return (
@@ -51,12 +83,17 @@ function AuthForm(props) {
                         <div className='InputFieldContainer'>
                             <label className='InputLabel'>Password Confirmation</label>
                             <input className='Field' type='password' placeholder='Password Confirmation' ref={passwordConfirmRef} required />
+                            <div className='PasswordError'>{passwordError}</div>
+                            {/* <div className='PasswordError'>{passwordConformationError}</div> */}
                         </div>}
+                    {/* {passwordError && <div>{passwordError}</div>} */}
+                    {/* <div>{passwordError}</div> */}
                     <div className='InputFieldContainer'>
                         <button disabled={loading} buttonStyle='btn--primary' type='submit'>{props.label}</button>
                     </div>
+                    {/* <div>{passwordError}</div> */}
                     <div className='AuthText'>
-                        {alternativeChoice} <Link to={alternativePath}>{alternativeLabel}</Link>
+                        {alternativeChoice} <Link to={alternativePath}>{alternativeLink}</Link>
                     </div>
                 </form>
             </div>
